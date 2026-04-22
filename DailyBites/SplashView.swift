71fb0d4fc19a47530a504a8eca230eb205bb461c
mareshbard <leticiapereirag59@gmail.com>
2026-04-22 12:05:11 -0300

    feat: add splash screen

---
+++ DailyBites/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  DailyBites
+//
+//  Created by USER on 22/04/26.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
