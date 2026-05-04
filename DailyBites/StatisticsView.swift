6e345c13e9929876278b19f94dbd09abd02dfbd3
Lucas Ibiapina <lucas.ibiapina04@gmail.com>
2026-05-04 12:05:58 -0300

    Feat: StatisticScreen finalizada

---
+++ DailyBites/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  DailyBites
+//
+//  Created by user on 28/04/26.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
