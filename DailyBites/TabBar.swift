9db1e43063fd8370e30a840bcd5c44c214f946c8
mareshbard <leticiapereirag59@gmail.com>
2026-05-05 09:23:17 -0300

    feat: add tabbar

---
+++ DailyBites/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  DailyBites
+//
+//  Created by USER on 05/05/26.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
