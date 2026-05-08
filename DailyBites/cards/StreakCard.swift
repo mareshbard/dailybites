e4feeba25d8971b8bdb9687e6eff2401bcf7e1d5
mareshbard <leticiapereirag59@gmail.com>
2026-05-08 12:06:17 -0300

    feat: add card streak

---
+++ DailyBites/cards/StreakCard.swift
@@ -0,0 +1,18 @@
+//
+//  StreakCard.swift
+//  DailyBites
+//
+//  Created by USER on 06/05/26.
+//
+
+import SwiftUI
+
+struct StreakCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StreakCard()
+}
